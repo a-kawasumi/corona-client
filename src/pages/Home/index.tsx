22e45802452038d7275865808d0748f5b96104a1
yumemi-kawasumi <a_kawasumi@yumemi.co.jp>
2022-01-08 17:28:50 +0900

    feat: create japan map component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { usePatient } from "../../hooks/usePatient";
 import { PatientGraph } from "../../components/PatientGraph";
+import { JapanMap } from "../../components/JapanMap";
+import styled from "styled-components";
+
+const Header = styled.div`
+  margin-top: 20px;
+`;
 
 export const Home: React.VFC = () => {
   const { graphData } = usePatient();
 
+  const handleSelect = useCallback((id: number) => {
+    console.log(id);
+  }, []);
+
   return (
     <div>
-      <h1>hello home</h1>
+      <Header />
+      <JapanMap onClickPrefecture={handleSelect} />
       {graphData && (
         <PatientGraph
           data={graphData.options}
